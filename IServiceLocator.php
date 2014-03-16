d3ed5d04dd242c49a36acb20a6a91ead94cd9b28
avreliy <avreliy.89@gmail.com>
2014-03-16 14:39:40 +0200

    Refactoring

---
+++ IServiceLocator.php
@@ -0,0 +1,41 @@
+<?php
+namespace CoreInterfaces;
+
+use ServiceLocator\Exceptions\ServiceLocatorException;
+use ServiceLocator\Utility\AdditionalParamsBehavior;
+use ServiceLocator\AbstractFactory;
+
+
+/**
+ * Class ServiceLocator
+ * Component require some auto loader
+ *
+ * @package ServiceLocator
+ * @property array           $registered
+ * @property AbstractFactory $abstractFactory
+ */
+interface IServiceLocator
+{
+    /**
+     * @param $className
+     *
+     * @throws \Exception
+     * @return Object
+     */
+    public function locate($className);
+
+    /**
+     * @param $className
+     *
+     * @throws ServiceLocatorException
+     * @return Object
+     */
+    public function createNewInstance($className);
+
+    /**
+     * @param array $config
+     */
+    public function __construct(array $config);
+
+    public function addAdditionalParam($alias, $value, $behavior);
+}
